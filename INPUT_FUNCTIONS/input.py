b8392da8fa252e2af4a882a90979dc8f505801e0
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-30 16:39:16 +0500

    Changes made

--- INPUT_FUNCTIONS/input.py
+++ INPUT_FUNCTIONS/input.py
@@ -1,1 +1,4 @@
-print("what is your name?")
+# print("what is your name?")
+input("What is your name?")
+# the input function pauses the program and waits for the user to type something
+# the input functino takes an optional argument, which is the prompt to display to the user
