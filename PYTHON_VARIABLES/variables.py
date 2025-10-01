c9472e5dbd8c27176aa0727e0e556376281d30db
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-01 18:36:51 +0500

    Changes made

--- PYTHON_VARIABLES/variables.py
+++ PYTHON_VARIABLES/variables.py
@@ -7,3 +7,7 @@ print(name)
 print(len(input("What is your name?")))
 # here we used three steps one is the input function to get the name from the user, second is the len function to check the length of the name 
 # and third is the print function to print the length of the name 
+# now we are going to split these three steps into three different lines of code
+name = input("What is your name?")
+Length = len(name)
+print(Length)
