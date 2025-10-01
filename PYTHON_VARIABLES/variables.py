d84207ef06da48f707bd2073776d829396d2ba10
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-01 18:34:33 +0500

    Changes made

---
+++ PYTHON_VARIABLES/variables.py
@@ -0,0 +1,9 @@
+# variables is a concept in programming to store data values. 
+# In python, variables are created when you assign a value to them.
+name = 'Arsal'
+print(name)
+# so here we created a variable called name and assigned it the value 'Arsal'
+# check the length of the variable name we use the len() function
+print(len(input("What is your name?")))
+# here we used three steps one is the input function to get the name from the user, second is the len function to check the length of the name 
+# and third is the print function to print the length of the name 
