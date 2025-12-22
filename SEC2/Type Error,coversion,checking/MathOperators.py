f8a1546e39e852ef23bce3157d1f831dea60b4cf
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-22 23:00:10 +0500

    Changes made

---
+++ SEC2/Type Error,coversion,checking/MathOperators.py
@@ -0,0 +1,8 @@
+# Mathematical Operators in python
+print(1+2)
+print(5-2)
+print(2*2)
+print(2/3)
+# Here the division will print float value because python here does the implicit type casting
+# so we use it to convert it to int
+print(2//3)
