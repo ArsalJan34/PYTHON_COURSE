1c69e1b9fbb1ddb962cfae5be9451a502ef382b0
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-22 22:27:45 +0500

    Changes made

---
+++ SEC2/Type Error,coversion,checking/TypeECC.py
@@ -0,0 +1,11 @@
+len("Hello")
+
+name = "hello"
+print(len(name))
+# this is how we check something that if its string or its another
+# datatype
+print(type("Hello"))
+
+print(type(123))
+print(type(0.2))
+print(type(True))
