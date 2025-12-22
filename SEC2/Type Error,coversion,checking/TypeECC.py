e7691286ce9e08b4981a33dd38ac4cc83e5ae0c8
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-22 22:43:27 +0500

    Changes made

--- SEC2/Type Error,coversion,checking/TypeECC.py
+++ SEC2/Type Error,coversion,checking/TypeECC.py
@@ -9,3 +9,16 @@ print(type("Hello"))
 print(type(123))
 print(type(0.2))
 print(type(True))
+
+# print("Number of letter in your name :")
+# name = (input("Enter your name: "))
+# print(len(name))
+
+name = (input("Enter your name here : "))
+Length = len(name)
+# print("The length of your name is : " + Length)
+# so the above code will give us error that can only concatenate str not int to str
+# lets check what data types are these
+print(type(name))
+print(type(Length))
+# so here it will print name as string and Length as the int
