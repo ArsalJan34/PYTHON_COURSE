b18039484752a4a36810bfe16a679e6aba09f32e
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-22 23:05:32 +0500

    Changes made

--- SEC2/Type Error,coversion,checking/MathOperators.py
+++ SEC2/Type Error,coversion,checking/MathOperators.py
@@ -6,3 +6,14 @@ print(2/3)
 # Here the division will print float value because python here does the implicit type casting
 # so we use it to convert it to int
 print(2//3)
+# but we only use this when  we need a digit which has no decimal values
+# and here is how we can get power of digits
+print(2**2)
+# Now this will print the power of 2 by 2 times
+# Python uses rule of PEDMAS
+# ()
+# **
+# /
+# *
+# +
+# -
