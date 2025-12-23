2a2194acc544f3f6ff9c1b9f4795c6e3eb773c7a
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-23 15:13:47 +0500

    Changes made

--- SEC1/INPUT_FUNCTIONS/input.py
+++ SEC1/INPUT_FUNCTIONS/input.py
@@ -3,6 +3,8 @@ input("What is your name?")
 # the input function pauses the program and waits for the user to type something
 # the input functino takes an optional argument, which is the prompt to display to the user
 #  if we print here ("hello arsal") but it doesnt know who is arsal so we use input function to get the name of the user
+
+
 print("helllo" + input("what is your name?"))
 # now that we run the program it will ask for the name and then it will print hello arsal
 # so now what we are gonna do is to treat this input function as an string because code its eventually gonna replaced by string
