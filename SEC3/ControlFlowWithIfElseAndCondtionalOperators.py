3700fdeb1fdbd0ed83646d01f2082e086eb8c5cf
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-23 15:31:08 +0500

    Changes made

---
+++ SEC3/ControlFlowWithIfElseAndCondtionalOperators.py
@@ -0,0 +1,7 @@
+print("Welcome to the RollerCoaster! ")
+height  = float(input("Enter Your Height in cm : "))
+if height > 120:
+  print("you are allowed to take a ride")
+else:
+  print("You have to grow taller to take a ride")
+
