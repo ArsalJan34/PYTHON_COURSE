f3bb47c07affd554491ea79c94f001b7cc008bac
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-23 13:32:35 +0500

    Changes made

---
+++ SEC2/Type Error,coversion,checking/NumManipulationAndFString.py
@@ -0,0 +1,12 @@
+# previously we created bmi but it printed values in float now we will try
+# to covert it into int and round the decimal value
+bmi = 54 / 1.78 ** 2
+print(bmi)
+print(int(bmi))
+# using int to convert is called as flooring in python
+print(round(bmi))
+# we can use round and add an ndigit to let it know how many digits we want after decimal
+print(round(bmi, 3))
+# It will give us 3 digits after the decimal point
+
+# Assignment operators in python
