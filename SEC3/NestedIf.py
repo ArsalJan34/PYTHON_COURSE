88480d4d5daa37f805f3da33b60cfcff9e8fb750
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-24 12:32:13 +0500

    Changes made

---
+++ SEC3/NestedIf.py
@@ -0,0 +1,6 @@
+print("Welcome to the RollerCoaster!")
+height = int(input("Enter Your Height : "))
+if height > 120:
+  print("You are welcome to enjoy the ride! ")
+else:
+  print("Sorry You have to grow taller to get the ride")
