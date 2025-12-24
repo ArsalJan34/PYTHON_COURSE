3c8bf3bc4e2dffe2d87fd116d2069eacb4a279d9
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-24 12:40:43 +0500

    Changes made

--- SEC3/NestedIf.py
+++ SEC3/NestedIf.py
@@ -2,5 +2,17 @@ print("Welcome to the RollerCoaster!")
 height = int(input("Enter Your Height : "))
 if height > 120:
   print("You are welcome to enjoy the ride! ")
+  age = int(input("Enter your age please : "))
+  if age <= 12:
+    print("you have to pay 5$")
+  elif age >= 12 & age <= 18:
+    print("You have to pay 7$")
+  else:
+    print("you have to pay 12$")
 else:
   print("Sorry You have to grow taller to get the ride")
+Welcome = input("")
+if Welcome == "Thank You":
+   print("You are welcome, Enjoy your Ride Sir")
+else:
+  print("Enjoy your Ride")
