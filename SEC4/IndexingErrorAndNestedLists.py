8664048930615f5de6d9109a1d5b3b69145ecd5c
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-25 19:14:03 +0500

    Changes made

---
+++ SEC4/IndexingErrorAndNestedLists.py
@@ -0,0 +1,30 @@
+Avengers = [
+    "Steve Rogers",
+    "Tony Stark",
+    "Natasha Romanoff",
+    "Bruce Banner",
+    "Clint Barton",
+    "Thor Odinson"
+]
+Number_Of_Avengers = len(Avengers)
+print(Avengers[Number_Of_Avengers -5])
+
+Avengers = [
+    "Steve Rogers",
+    "Tony Stark",
+    "Natasha Romanoff",
+    "Bruce Banner",
+    "Clint Barton",
+    "Thor Odinson"
+]
+
+Villains = [
+    "Thanos",
+    "Loki",
+    "Ultron",
+    "Red Skull",
+    "Green Goblin",
+    "Hela"
+]
+MarvelMovie = [Villains, Avengers]
+print(MarvelMovie)
