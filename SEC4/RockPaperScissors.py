8eba43442c92da599c3b71de8d418f5e4e7d2a80
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-26 22:46:28 +0500

    Changes made

--- SEC4/RockPaperScissors.py
+++ SEC4/RockPaperScissors.py
@@ -2,7 +2,11 @@ import random
 user = int(input("What do you choose , 0 Rock , 1 Paper, 2 Scissors : "))
 computer = random.randint(0,2)
 print(f"Computer choice  : {computer}  ")
-if user > computer:
+if user == 0 and computer == 2:
   print("You won")
-else:
+elif computer > user:
   print("You lost")
+elif computer == user:
+  print("Its a draw")
+else:
+  print("you entered invalid input")
