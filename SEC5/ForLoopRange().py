e162f0c6a9caa2ecf02cc606aaffa61438ad7326
ArsalJan34 <arsaljaan532@gmail.com>
2025-12-31 15:52:39 +0500

    Changes made

---
+++ SEC5/ForLoopRange().py
@@ -0,0 +1,11 @@
+#range function
+for number in  range(1,10):
+  print(number)
+#but it will print numbers between 1 to 9
+for number in range(1,11):
+  print(number)
+#now this will print numbers from 1 to 10
+for number in range(1,11,3):
+  print(number)
+for number in range(1,101):
+  print(number)
