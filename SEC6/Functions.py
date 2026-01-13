f01f68ca19c02433e72e298d911ea06c4c572eda
ArsalJan34 <arsaljaan532@gmail.com>
2026-01-13 16:15:45 +0500

    Changes made

---
+++ SEC6/Functions.py
@@ -0,0 +1,27 @@
+# we are creating or defining a function
+def my_funtion():
+  print("Hello")
+  print("Bye")
+
+# and this is how we call the function
+my_funtion()
+# i created an function is reeborgs word
+# and completed an square round with function
+def turn_around():
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+
+turn_around()
