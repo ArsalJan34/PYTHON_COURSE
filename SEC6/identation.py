074a5ec5bed5146c01e88c90e86b855e3ae389c1
ArsalJan34 <arsaljaan532@gmail.com>
2026-01-14 20:48:43 +0500

    Changes made

---
+++ SEC6/identation.py
@@ -0,0 +1,16 @@
+age = 20
+
+# This if statement checks the age
+if age >= 18:
+    # Everything indented here belongs to the if block
+    print("You are an adult")
+
+    # This line is STILL inside the if block
+    print("You can vote")
+else:
+    # This block runs only if the if condition is False
+    print("You are under 18")
+
+# This line is OUTSIDE the if-else block
+print("Program finished")
+# If a line ends with : → next line MUST be indented
