f33eccf74a07441a9961b9233a02d39cfaff0e05
ArsalJan34 <arsaljaan532@gmail.com>
2026-01-16 00:48:17 +0500

    Changes made

---
+++ SEC6/IfElseandWhileLoop.py
@@ -0,0 +1,23 @@
+def turn_right():
+ turn_left()
+ turn_left()
+ turn_left()
+
+def jump():
+#  we are going to remove the move function first move function because it if there is a wall it will still move
+#  move()
+ turn_left()
+ move()
+ turn_right()
+ move()
+ turn_right()
+ move()
+ turn_left()
+
+
+while not at_goal():
+  if wall_in_front():
+   jump()
+  else:
+   move()
+#
--- SEC6/WhileLoop.py
+++ SEC6/WhileLoop.py
@@ -34,3 +34,4 @@ while not at_goal():
 # in simple words A while loop in Python is used to repeat a block of code as long as a given condition is True.
 # The loop keeps running until the condition becomes False.
 # when the false is not existing it will become an infinite loop
+# like example while 5 > 3
