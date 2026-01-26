1d61acc70e5c655516dc87397168b303221d19d7
ArsalJan34 <arsaljaan532@gmail.com>
2026-01-26 15:52:43 +0500

    key arguments

--- SEC8/PositionalKeyWordArgument.py
+++ SEC8/PositionalKeyWordArgument.py
@@ -6,3 +6,12 @@ def greet_with(name, location):
 greet_with("arsal", "new york")
 # or
 # greet_with(name:"arsal", location:"new york")
+
+# KEY Arguments
+def my_function(a,b,c):
+  print(f"a={a}")
+  print(f"b={b}")
+  print(f"c={c}")
+
+my_function(a=1,b=2,c=3)
+my_function(1,2,3)
