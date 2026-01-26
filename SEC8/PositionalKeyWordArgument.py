4e224ae0730dc08e55d0dd494189a207ffdb5d40
ArsalJan34 <arsaljaan532@gmail.com>
2026-01-26 15:33:03 +0500

    Section8_3

--- SEC8/PositionalKeyWordArgument.py
+++ SEC8/PositionalKeyWordArgument.py
@@ -3,4 +3,6 @@ def greet_with(name, location):
   print(f"hello {name}")
   print(f"what is it like in {location}")
 
-greet_with("arsal", "hyderabad")
+greet_with("arsal", "new york")
+# or
+# greet_with(name:"arsal", location:"new york")
