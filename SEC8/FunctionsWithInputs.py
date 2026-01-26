d21bd960acf1a3b437b05cbcf0c74b9e31eb323a
ArsalJan34 <arsaljaan532@gmail.com>
2026-01-26 14:53:52 +0500

    Section8_2

--- SEC8/FunctionsWithInputs.py
+++ SEC8/FunctionsWithInputs.py
@@ -4,3 +4,9 @@ def greet():
   print("what were you doing?")
 
 greet()
+
+def greet_with_name(name):
+  print(f"hello {name}")
+  print(f"how do you do {name}")
+
+greet_with_name("arsal jani")
---
+++ SEC8/weeksleftinlife.py
@@ -0,0 +1,7 @@
+def life_in_weeks(age):
+    weeksinyear = 52
+    maxage = 90
+    weeksleft = (maxage - age) * weeksinyear
+    print(f"you have {weeksleft}")
+
+life_in_weeks(18)
