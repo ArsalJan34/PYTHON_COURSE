1bc681efcc4ee8d798f698b26cbae15c29f0eba8
ArsalJan34 <arsaljaan532@gmail.com>
2026-02-19 16:44:18 +0500

    Changes made

--- SEC8/CaesarCypher2.py
+++ SEC8/CaesarCypher2.py
@@ -7,25 +7,25 @@ shift = int(input("Type the shift number: \n"))
 # creating a function called encrypt() that takes originalText and shiftAmount as 2 inputs
 # Inside the encrypt() function shift each letter of the orginalText forwards in the alphabet
 # by shift amount and print the encrypt text.
-def encrypt(originalText, shiftAmount):
-  cipherText = ""
-  for letter in originalText:
-    shiftedPosition = alphabet.index(letter) + shiftAmount
-    # here we assigned old shiftedPosition to modulo of alphabet length
-    shiftedPosition = shiftedPosition % len(alphabet)
-    cipherText += alphabet[shiftedPosition]
-  print(f"Here is the encoded result: {cipherText}")
-encrypt(originalText=text, shiftAmount=shift)
+# def encrypt(originalText, shiftAmount):
+#   cipherText = ""
+#   for letter in originalText:
+#     shiftedPosition = alphabet.index(letter) + shiftAmount
+#     # here we assigned old shiftedPosition to modulo of alphabet length
+#     shiftedPosition = shiftedPosition % len(alphabet)
+#     cipherText += alphabet[shiftedPosition]
+#   print(f"Here is the encoded result: {cipherText}")
+# encrypt(originalText=text, shiftAmount=shift)
 
-def decrypt(originalText, shiftAmount):
-  cipherText2 = ""
-  for letter in originalText:
-    shiftedPosition = alphabet.index(letter) - shiftAmount
-    shiftedPosition % len(alphabet)
-    cipherText2 += alphabet[shiftedPosition]
+# def decrypt(originalText, shiftAmount):
+#   cipherText2 = ""
+#   for letter in originalText:
+#     shiftedPosition = alphabet.index(letter) - shiftAmount
+#     shiftedPosition % len(alphabet)
+#     cipherText2 += alphabet[shiftedPosition]
 
-  print(f"Here is the encoded result: {cipherText2}")
-decrypt(originalText=text, shiftAmount=shift)
+#   print(f"Here is the encoded result: {cipherText2}")
+# decrypt(originalText=text, shiftAmount=shift)
 
 
 def caesar(originalText, shiftamount, encode_or_decode):
@@ -34,9 +34,12 @@ def caesar(originalText, shiftamount, encode_or_decode):
 
     if encode_or_decode == "decode":
         shiftamount *= -1
-    shiftedPosition = alphabet.index(letter) - shiftAmount
+    if encode_or_decode == "encode":
+        shiftamount *= 1
+    shiftedPosition = alphabet.index(letter) - shiftamount
     shiftedPosition % len(alphabet)
     cipherText2 += alphabet[shiftedPosition]
 
-  print(f"Here is the encoded result: {cipherText2}")
-decrypt(originalText=text, shiftAmount=shift)
+  print(f"Here is the {encode_or_decode}d result: {cipherText2}")
+# decrypt(originalText=text, shiftAmount=shift)
+caesar(originalText=text, shiftamount=shift, encode_or_decode=direction)
