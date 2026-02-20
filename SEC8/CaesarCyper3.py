8669f17d4858f7ec257e3061af22d0343e846e64
ArsalJan34 <arsaljaan532@gmail.com>
2026-02-20 11:16:38 +0500

    Changes made

--- SEC8/CaesarCyper3.py
+++ SEC8/CaesarCyper3.py
@@ -1,26 +1,27 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-
-
-
-
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
 def caesar(originalText, shiftamount, encode_or_decode):
-  OutputText = ""
-  for letter in originalText:
-    if letter not in alphabet:
-       OutputText +=letter
-    else:
-        if encode_or_decode == "decode":
-            shiftamount *= -1
-        if encode_or_decode == "encode":
-            shiftamount *= 1
-        shiftedPosition = alphabet.index(letter) - shiftamount
-        shiftedPosition % len(alphabet)
-        OutputText += alphabet[shiftedPosition]
+    OutputText = ""
 
-  print(f"Here is the {encode_or_decode}d result: {OutputText}")
-# decrypt(originalText=text, shiftAmount=shift)
+    # normalize large shifts
+    shiftamount %= len(alphabet)
+
+    # flip shift for decoding only once (fixed)
+    if encode_or_decode == "decode":
+        shiftamount = -shiftamount
+
+    for letter in originalText:
+        if letter not in alphabet:
+            OutputText += letter
+        else:
+            shiftedPosition = alphabet.index(letter) + shiftamount
+            shiftedPosition %= len(alphabet)
+            OutputText += alphabet[shiftedPosition]
+
+    print(f"Here is the {encode_or_decode}d result: {OutputText}")
+
 should_continue = True
 
 while should_continue:
@@ -28,11 +29,10 @@ while should_continue:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message: \n").lower()
     shift = int(input("Type the shift number: \n"))
+
     caesar(originalText=text, shiftamount=shift, encode_or_decode=direction)
-restart = input("Type YES if you want to go again, Or Type NO if you want to quit").lower()
-if restart == "no":
-   should_continue = False
-else:
-   print("GoodBye Sir")
 
-
+    restart = input("Type YES if you want to go again, or NO if you want to quit:\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("GoodBye Sir")
