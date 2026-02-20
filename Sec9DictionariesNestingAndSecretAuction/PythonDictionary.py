75004fbbdde381edbc8ebc4d1e93dd319af080ec
ArsalJan34 <arsaljaan532@gmail.com>
2026-02-20 11:49:12 +0500

    Changes made

---
+++ Sec9DictionariesNestingAndSecretAuction/PythonDictionary.py
@@ -0,0 +1,6 @@
+programing_dictionaries = {
+"Bug":"An Error in program which prevents program from running as expected",
+"Function": "A piece of code which can be called again and again if needed",
+"Loop":"The Action of doing something over and over again"
+}
+print(programing_dictionaries["Bug"])
