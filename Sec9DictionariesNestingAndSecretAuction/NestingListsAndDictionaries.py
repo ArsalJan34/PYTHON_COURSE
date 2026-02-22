29aab0ca9ee0cc89cc76ad16891d97d448435a9e
ArsalJan34 <arsaljaan532@gmail.com>
2026-02-22 16:36:52 +0500

    Changes made

---
+++ Sec9DictionariesNestingAndSecretAuction/NestingListsAndDictionaries.py
@@ -0,0 +1,16 @@
+capitals = {
+"France":"Paris",
+"Germany": "Berlin"
+}
+
+# nested list in dictionary
+
+travel_log = {
+  "France":["Paris","Lille", "Dijon"],
+  "Germany":["Stutgratt","Berlin"]
+}
+
+print(travel_log["France"][1])
+
+nested_list = ["A","B",["C","D"]]
+print(nested_list[2])
