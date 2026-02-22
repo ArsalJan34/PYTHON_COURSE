d460fdb3c3a6342be115e6035474fa7c5bc046a4
ArsalJan34 <arsaljaan532@gmail.com>
2026-02-22 17:05:10 +0500

    Changes made

--- Sec9DictionariesNestingAndSecretAuction/NestingListsAndDictionaries.py
+++ Sec9DictionariesNestingAndSecretAuction/NestingListsAndDictionaries.py
@@ -9,8 +9,23 @@ travel_log = {
   "France":["Paris","Lille", "Dijon"],
   "Germany":["Stutgratt","Berlin"]
 }
-
+# to print lille we will
 print(travel_log["France"][1])
 
 nested_list = ["A","B",["C","D"]]
-print(nested_list[2])
+# to print d
+print(nested_list[2][1])
+
+travel_log = {
+  "France" : {
+    "cities_visited":["paris", "Dijon", "Lille"],
+    "total_visits": 5
+},
+  "Germany": {
+    "cities_visited":["Berlin", "Stutgratt", "Humburg"],
+    "total_visits":5
+
+  }
+
+}
+print(travel_log["Germany"])
