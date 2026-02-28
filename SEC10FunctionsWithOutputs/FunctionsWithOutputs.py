13e7bb39566611ad3792db88231c25f53089c587
ArsalJan34 <arsaljaan532@gmail.com>
2026-02-28 21:23:36 +0500

    Changes made

---
+++ SEC10FunctionsWithOutputs/FunctionsWithOutputs.py
@@ -0,0 +1,10 @@
+# def format_name():
+#   first_name = input("Enter Your first name : ")
+#   last_name = input("Enter Your Last name : ")
+#   print(f"hi mr {first_name} {last_name}")
+
+# format_name()
+
+def format_name(f_name, l_name):
+
+format_name()
