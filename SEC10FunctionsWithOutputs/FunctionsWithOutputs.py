a3028f0dd35538822460b7eb85db403081431881
ArsalJan34 <arsaljaan532@gmail.com>
2026-03-28 00:53:05 +0500

    continued course after some days

--- SEC10FunctionsWithOutputs/FunctionsWithOutputs.py
+++ SEC10FunctionsWithOutputs/FunctionsWithOutputs.py
@@ -10,5 +10,6 @@ def format_name(f_name, l_name):
   # print(l_name.title())
   Formated_f_name = f_name.title()
   Formated_l_name = l_name.title()
-  print(f"{Formated_f_name}\n{Formated_l_name}")
-format_name("angela","ANGELA")
+  # return f"{Formated_f_name} {Formated_l_name}"
+  print(f"{Formated_f_name} {Formated_l_name}")
+format_name("AnGelA","ANGELA")
